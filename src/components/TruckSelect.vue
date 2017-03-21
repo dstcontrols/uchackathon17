b7561073da094d0b5e40728e91b3b8b100cf3d86
Jerry Vin <jvin@dstcontrols.com>
2017-03-20 17:59:22 -0700

    TruckSelect working but broken.

---
+++ src/components/TruckSelect.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="TruckSelect">
+        <b-form-select v-model="selected" 
+                    :options="options"
+                    :default-option="defaultOption"
+                    calss="mb-3"
+        ></b-form-select>
+    </div>
+</template>
+
+<script type = "text/javascript" >
+
+export default {
+  name: 'TruckSelect',
+  data: {
+    selected: 'default',
+    defaultOption: {
+      text: 'Please select some item',
+      value: 'default',
+    },
+    options: [{
+      text: 'This is First option',
+      value: 'a',
+    }, {
+      text: 'Default Selected Option',
+      value: 'b',
+    }, {
+      text: 'This is another option',
+      value: 'c',
+    }, {
+      text: 'This one is disabled',
+      value: 'd',
+      disabled: true,
+    }],
+  },
+  props: 
+};
+</script>
+
+<style>
+</style>
