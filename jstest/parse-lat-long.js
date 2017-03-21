a54d5a2fb8d96d9ecb820c6b6c73cceea003a199
Jerry Vin <jvin@dstcontrols.com>
2017-03-20 18:53:34 -0700

    parseLatLong function working

--- jstest/parse-lat-long.js
+++ jstest/parse-lat-long.js
@@ -7,9 +7,9 @@ function parseLatLong(apiCallResults) {
     for (var i_truck = 0; i_truck < numTrucks; i_truck++)
     {
         mapDataOut.push({
-            numTruck: 400 + i_truck,
-            latitude: apiCallResults.LatValues.Content.Items[i_truck].Content.Value,
-            longitude: apiCallResults.LngValues.Content.Items[i_truck].Content.Value,
+            numTruck: 401 + i_truck,
+            latitude: apiCallResults.LatValues.Content.Items[i_truck].Content.Value/3600000,
+            longitude: apiCallResults.LngValues.Content.Items[i_truck].Content.Value/3600000,
         });
     }
 
