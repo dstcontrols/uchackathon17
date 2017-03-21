006200d67f28f85f817980d4254c11b404e09692
Jerry Vin <jvin@dstcontrols.com>
2017-03-20 22:57:53 -0700

    Color severity functions added for a couple oil pressure metrics

--- jstest/color-severity.js
+++ jstest/color-severity.js
@@ -1,4 +1,9 @@
-// Takes in a DeltaExhaust value in deg C from the ENG LOAD attribute
+// hex color database
+var hexColorSafe = "#1a237e";
+var hexColorWarning = "#ffab00";
+var hexColorDanger = "#b71c1c";
+
+// Takes in a DeltaExhaust value in deg C from the RT-LT EXH TEMP attribute
 // Outputs a hex color depending on severity
 function EvaluateDeltaExhaust (deltaT) {
     var hexColorOut;
@@ -6,15 +11,48 @@ function EvaluateDeltaExhaust (deltaT) {
 
     // Blue if good
     if (absDeltaT <= 20) {
-        hexColorOut = "#1a237e";
+        hexColorOut = hexColorSafe;
     }
     // Amber if warning
     else if (absDeltaT > 20 && absDeltaT <= 50 ) {
-        hexColorOut = "#ffab00";
+        hexColorOut = hexColorWarning;
     }
     // Red if danger
     else {
-        hexColorOut = "#b71c1c";
+        hexColorOut = hexColorDanger;
     }
     return hexColorOut;
 }
+
+// Takes in Engine Oil Pressure in kPa from the ENG OIL PRES attribute
+// Outputs a hex color depending on severity
+function EvaluateEngineOilPressure(P_Oil_KPa) {
+    var hexColorOut;
+    var P_Oil_psi = P_Oil_KPa * 0.145038;
+
+    if (P_Oil_psi > 87 || P_Oil_psi < 31) {
+        hexColorOut = hexColorWarning;
+    }
+    else {
+        hexColorOut = hexColorSafe;
+    }
+
+    return hexColorOut;
+}
+
+// Takes in Boost Pressure in psi from the BOOST PRES attribute
+// Outputs a hex color depending on severity
+function EvaluateBoostPressure(P_Boost) {
+    var hexColorOut;
+
+    if (P_Boost > 27 || P_Boost < 21) {
+        hexColorOut = hexColorWarning;
+    }
+    else {
+        hexColorOut = hexColorSafe;
+    }
+
+    return hexColorOut;
+}
+
+// Takes in Delta Brake Temperature from the front or rear from RTF-LTF BRKTEMP
